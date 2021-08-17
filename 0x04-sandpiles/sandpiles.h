5e186671a98266248304c6ddef07be89e8829680
JwowSquared <Jdwagner1@hotmail.com>
2021-08-17 12:19:55 +0000

    added header

---
+++ 0x04-sandpiles/sandpiles.h
@@ -0,0 +1,9 @@
+#ifndef _SANDPILES_H_
+#define _SANDPILES_H_
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void add_piles(int grid1[3][3], int grid2[3][3]);
+int check_stable(int grid[3][3]);
+void ripped_print_grid(int grid[3][3]);
+
+#endif /* _SANDPILES_H_ */
