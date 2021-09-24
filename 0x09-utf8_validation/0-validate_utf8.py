950d2f83166d97a88be4e07ad603ac23c2370388
JwowSquared <Jdwagner1@hotmail.com>
2021-09-24 00:21:25 +0000

    added basic source to run against checker

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" this module contains the validUTF8 function """
+
+
+def validUTF8(data):
+    """ determines if list of integers is UTF8 compliant """
+    for i in data:
+        print("i = {:d}".format(i))
+    return True
