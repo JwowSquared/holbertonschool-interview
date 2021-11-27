0732c16162d4a48c7df726f56a21d64068a31b9f
JwowSquared <Jdwagner1@hotmail.com>
2021-11-27 22:51:05 +0000

    added solutions

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,6 +3,35 @@
 import sys
 
 
+def gen_queens(solutions, n, current, depth):
+    """x"""
+    if depth >= n:
+        solutions.append(current.copy())
+        return
+    for i in range(n):
+        if i in current:
+            continue
+        current[depth] = i
+        if depth > 0:
+            d_pos = [x + current[x] for x in range(depth + 1)]
+            d_neg = [x - current[x] for x in range(depth + 1)]
+            if len(set(d_pos)) != depth + 1:
+                continue
+            if len(set(d_neg)) != depth + 1:
+                continue
+        gen_queens(solutions, n, current, depth + 1)
+    current[depth] = -1
+
+
+def n_queens(n):
+    """calls gen_solutions with n and formats result"""
+    solutions = []
+    current = [-1 for x in range(n)]
+    gen_queens(solutions, n, current, 0)
+    for s in solutions:
+        print([[x, s[x]] for x in range(n)])
+
+
 def main():
     """prints nqueens where n is the first argv"""
     if len(sys.argv) is not 2:
@@ -14,7 +43,7 @@ def main():
     if int(sys.argv[1]) < 4:
         print("N must be at least 4")
         return 1
-    print("poggers")
+    n_queens(int(sys.argv[1]))
 
 if __name__ == "__main__":
     main()
