7c1695e2820c018da731b297f1bd8101afc42cd5
JwowSquared <Jdwagner1@hotmail.com>
2021-12-03 03:04:59 +0000

    added 1 to size on a left slice

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -42,7 +42,7 @@ int advanced_binary(int *array, size_t size, int value)
 			return (mid);
 	}
 	else if (value < array[mid])
-		out = advanced_binary(array, mid, value);
+		out = advanced_binary(array, mid + 1, value);
 	else
 		out = advanced_binary(array + mid + 1, size - mid - 1, value);
 
