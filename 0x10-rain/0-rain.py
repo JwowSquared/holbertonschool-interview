c7968d955e9f719439e156cdfa4bd5439f9032a8
JwowSquared <Jdwagner1@hotmail.com>
2021-12-04 19:24:25 +0000

    added task 0 script

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,34 @@
+#!/bin/usr/python3
+"""this module holds the rain function"""
+
+
+def rain(walls):
+    """calculates the amount of rainwater left in walls"""
+    if not walls:
+        return 0
+
+    i = 0
+    out = 0
+    height = -1
+    left_wall = -1
+    right_wall = -1
+    while i < len(walls):
+        if walls[i] == 0:
+            i += 1
+            continue
+        if left_wall == -1:
+            left_wall = i
+            i += 1
+            continue
+        if right_wall == -1:
+            right_wall = i
+            if walls[left_wall] < walls[right_wall]:
+                height = walls[left_wall]
+            else:
+                height = walls[right_wall]
+            out += (height * (right_wall - left_wall - 1))
+            left_wall = right_wall
+            right_wall = -1
+            i += 1
+            continue
+    return out
